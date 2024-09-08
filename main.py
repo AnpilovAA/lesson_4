cb6db7bb081c89ebab37e49565cb014bc7046a7e
AnpilovAA <a.anpilov93@gmail.com>
2024-09-08 10:56:31 +0300

    исправил алгоритм

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ from PIL import Image
 
 image = "Merlyn_Monroe.png"
 
+pixel_size_for_cutting = 96
 
-def extract_canals_from_img() -> object:
+half_cutting_size = 96//2
+
+
+def image_canals_split() -> object:
     with Image.open(image) as img:
         red, green, blue = img.split()
     return red, green, blue
@@ -12,35 +16,38 @@ def extract_canals_from_img() -> object:
 
 def layers_displacement(*args) -> tuple:
     red, green, blue = args[0]
-
-    coordinates = (25, 0, 696, 522)
-    croped_blue_canal = blue.crop(coordinates)
-
-    coordinates_red = (0, 0, 671, 522)
-    croped_red_canal = red.crop(coordinates_red)
-
-    red_layer = Image.blend(croped_blue_canal, croped_red_canal, 0.3)
     
-    coordinates = (0, 0, 671, 522)
-    croped_blue_canal = blue.crop(coordinates)
-
-    coordinates_red = (25, 0, 696, 522)
-    croped_red_canal = red.crop(coordinates_red)
-
-    blue_layer = Image.blend(croped_blue_canal, croped_red_canal, 0.3)
+    red_layer = channel_cutting(red, 1)
+    blue_layer = channel_cutting(blue, 0)
+    
+    coordinates = (half_cutting_size, 0, 696-half_cutting_size, 522)
+    green_layer = green.crop(coordinates)
    
-    coordinates = (25, 0, 696, 522)
-    croped_green_canal = green.crop(coordinates)
-    coordinates_red = (0, 0, 671, 522)
-    croped_red_canal = red.crop(coordinates_red)
-
-    green_layer = Image.blend(croped_red_canal, croped_green_canal, 0.3)
     return red_layer, blue_layer, green_layer
+
+
+def channel_cutting(canal, key):
+    cut_size = canal.size[0] - half_cutting_size
+    cutted_left_right = canal.crop((half_cutting_size, 0, cut_size, 522))
+    
+    if key:
+        coordinates = (pixel_size_for_cutting, 0, 696, 522)
+        cutted_left_red_canal = canal.crop(coordinates)
+
+        return Image.blend(cutted_left_red_canal, cutted_left_right, 0.3)
+    else:
+        cutting_blue_canal = 696-pixel_size_for_cutting
+        coordinates = (0, 0, cutting_blue_canal, 522)
+        cutted_rigth_blue_canal = canal.crop(coordinates)
+
+        return Image.blend(cutted_rigth_blue_canal, cutted_left_right, 0.3)
+
+
 
 
 def merge_all_layers(*args):
     red_layer, blue_layer, green_layer = args[0]
-    super_image = Image.merge('RGB', (green_layer, blue_layer, red_layer))
+    super_image = Image.merge('RGB', (red_layer, green_layer, blue_layer))
     super_image.save(f"Style_{image.replace(".png", "_new")}.jpg")
     
     super_image.thumbnail((80, 80))
@@ -48,6 +55,6 @@ def merge_all_layers(*args):
 
 
 if __name__ == "__main__":
-    rgb = extract_canals_from_img()
+    rgb = image_canals_split()
     layers = layers_displacement(rgb)
     merge_all_layers(layers)
